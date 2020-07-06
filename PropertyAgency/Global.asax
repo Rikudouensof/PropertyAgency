910b93ba9a05f460d39aaf97e7160cf31fc734c0
Rikudou En Sof <rikudouensof@outlook.com>
2020-07-06 20:41:04 +0100

    Add project files.

---
+++ PropertyAgency/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PropertyAgency.MvcApplication" Language="C#" %>
---
+++ PropertyAgency/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PropertyAgency.Startup))]
+namespace PropertyAgency
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
